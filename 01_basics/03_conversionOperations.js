d3e899df7e66369d5a632ba88d71211201fc728e
arpitshrivastava08 <arpitshrivastava9377@gmail.com>
2025-01-18 17:10:57 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,4 @@
+/*
 let score = "33"
 
 //const {score} = req.body 
@@ -10,6 +11,7 @@ let valueInNumber = Number(score)
 // as we seen in terminal if the data type is int then o/p is "number" 
 //and to typecast it into number we have to write "Number"
 console.log(typeof valueInNumber); 
+*/
 
 /*
 score = "33abc"
@@ -60,7 +62,55 @@ console.log(booleanIsLoggedIn); // true
 // 1 => true; 0 => false
 // "" => false; "Hitesh" => true
 
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber); 
 console.log(typeof stringNumber); 
+*/
+
+// **************OPERATIONS**************
+
+/*
+let value = 3
+let negvalue = -value
+// console.log(negvalue); 
+
+// console.log(2+2); 
+// console.log(2-2); 
+// console.log(2*2); 
+// console.log(2**3);
+// console.log(2/3); 
+// console.log(2%3); 
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3); 
+
+console.log("1" + 2);  // 12
+console.log(1 + "2");  // 12
+console.log("1" + 2 + 2);  // 122
+console.log(1 + 2 + "2");  // 32
+
+// Always use parenthesis why messy code?
+console.log((3+4) * 5 % 3); 
+*/
+
+// console.log(+true);  // o/p 1
+// console.log(true+);   //o/p error
+// console.log(+"");  //o/p 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1)
+
+let gameCounter = 100
+++gameCounter; 
+console.log(gameCounter);
+
+
+// link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.
+// html#sec-type-conversion 
