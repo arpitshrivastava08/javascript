535b11d903576eafaafebb6183db0b93a1726111
arpitshrivastava08 <arpitshrivastava9377@gmail.com>
2025-01-24 14:41:11 +0000

    jsone

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if
+
+if(true) {
+    // excute hoga 
+}
+if(false){
+    // execute nhi hoga 
+}
+
+const isUserLoggedIn = true; 
+if(isUserLoggedIn){
+}
+
+// <, >, <=, >=, ==, !=, ===
+if( 2 == "2") {
+    console.log("exexuted"); 
+} // exexuted
+
+if( 2 === "2") {
+    console.log("executed"); 
+}  // nothing return 
+
+const temperature = 41
+if(temperature < 50){
+    console.log("less than 50");
+}
+else{
+    console.log("temperature is greater than 50"); 
+}
