27247c4f3d3bb5b80e6ac45fe041c9f73a1b0ba1
arpitshrivastava08 <arpitshrivastava9377@gmail.com>
2025-01-25 13:16:44 +0000

    dom

---
+++ 05_iterations/eight.js
@@ -0,0 +1,43 @@
+const array1 = [1, 2, 3, 4]; 
+
+/*
+// 0 + 1 + 2 + 3 + 4 
+const initailvalue = 0; 
+const sumWithInitial = array1.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, initailvalue
+);
+
+console.log(sumWithInitial); 
+// 10
+*/
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc : ${acc} and currval : ${currval}`); 
+//     return acc + currval
+// }, 0)
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+console.log(myTotal); 
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+    },
+    {
+        itemName : "py course",
+        price : 3999
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+    },
+    {
+        itemName : "data science course",
+        price : 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay);
